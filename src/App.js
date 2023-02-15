8f78bd8352c197e79be7a81bcb0d9fdf09cd5e59
Lucas Torres <lucascms11@gmail.com>
2023-02-15 18:09:49 -0300

    [FIX] Retirando variáveis não usadas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { OPEN_APP } from './utils/urls.js';
 import { Loading } from './components/Loading/Loading';
 import { Links } from './components/Links/Links';
 
