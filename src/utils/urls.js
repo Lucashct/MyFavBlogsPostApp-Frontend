1e7b8226b7a6ceecae9ad3c51def6c6c71b4589b
Lucas Torres <lucascms11@gmail.com>
2023-02-15 14:49:21 -0300

    [FIX] Corrigindo url produção

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
 //const BASE_URL = 'http://localhost:4000';
-const BASE_URL = 'myfavblogspostapp-backend-production.up.railway.app';
+const BASE_URL = 'http://myfavblogspostapp-backend-production.up.railway.app';
 
 export const OPEN_APP = BASE_URL + '/'
 export const LIST_LINKS = BASE_URL + '/links';
