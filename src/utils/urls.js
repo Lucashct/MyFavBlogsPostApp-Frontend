bb6a259411dafbd185529c89451b2bbba729085d
Lucas Henrique C. Torres <62104459+Lucashct@users.noreply.github.com>
2023-02-17 10:21:58 -0300

    [

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
 //const BASE_URL = 'http://localhost:4000';
-const BASE_URL = 'https://myfavblogsposts.onrender.com';
+const BASE_URL = 'https://myfavblogspostapp-backend-production.up.railway.app';
 
 export const OPEN_APP = BASE_URL + '/'
 export const LIST_LINKS = BASE_URL + '/links';
